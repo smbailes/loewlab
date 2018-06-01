dece71507b33b42e435148ec86fb6e5b8ad47fd6
smbailes <39807025+smbailes@users.noreply.github.com>
2018-06-01 11:06:45 -0400

    Added pathfinder function
    Function to efficiently find location of patient images

---
+++ Symmetry Code (Final)/pathfinder.m
@@ -0,0 +1,13 @@
+function [location, ptID] = choosedialog
+%% Patient or Volunteer
+    answer = questdlg('ID Patient Type:','Patient Type','Patient','Volunteer','Patient');
+    if (strcmp(answer, 'Patient'))
+        ptID = patientselect; %return patient ID
+    else
+        ptID = volunteerselect; %return patient ID
+    end
+    
+%% File Explorer 
+    dir = uigetdir; %file exploerer to find image directory
+    location = strcat(dir, '\', ptID, '\Cropped Image\');
+end
