bd3bff6c4b2f16020188c79795df23b86ed7f80e
smbailes <39807025+smbailes@users.noreply.github.com>
2018-06-06 16:58:28 -0400

    Fixed RegisteredCropScript

--- Symmetry Code (Final)/RegisteredCropScript.m
+++ Symmetry Code (Final)/RegisteredCropScript.m
@@ -1,35 +1,21 @@
 % function newcrop = RegisteredCropScript(location)
 
-<<<<<<< HEAD
-[location, ptID] = pathfinder;
-
-location = 'F:\IRST012\IRST012\' %comment this part out 
-a=inputdlg('Enter first image number: '); 
-strt=a{1};  
-strt = str2double(strt); 
-for i=1:15          
-=======
 path = uigetdir;
 location = strcat(path, '\');
 
 strt = 120;
 for i=1:14          
->>>>>>> registration
     I_mat{i} = imread([location sprintf('%04d.tif',strt)]);    % Read each image into I_mat
     strt=strt+120;            % Go to next image (for cropped), HAS TO BE CHANGED TO INCREMENT BY 1
 end
 
 %% Apply Crop to All Registered Images based on User Drawn Input
 % cd ([homedir 'Registered/' ptID 'Registered/']);
-<<<<<<< HEAD
-firstImage = I_mat{1};
-=======
 image = I_mat{7};
 I = getMatrixOutliers(image);
 nonzero = I(find(I>0));
 h = max(nonzero);
 l = min(nonzero);
->>>>>>> registration
 
 figure
 set(gcf,'units','inches', 'Position',[4 2 10 8])
@@ -68,37 +54,17 @@ newLocation = strcat(location, '\', 'Cropped');
 mkdir(newLocation);
 cd(newLocation);
 
-<<<<<<< HEAD
-firstImage = firstImage(find(firstImage>0));    % Remove zero pixels
-low = min(firstImage(:)); %finds lowest intensity pixel
-high = max(firstImage(:)); %finds highest intensity pixels
-
-imwrite(newCrop,'0000.tif');
-imshow(newCrop, [low high]) %displays image with set low and high intensity values
-
-x = 120; 
-
-for i = 1:1:15
-    cd ..
-    newImage = I_mat{i};
-=======
 imwrite(newCrop,'0120.tif');
 
 for i = 240:120:1680
     cd(location)
     newImage = imread(sprintf('%04d.tif',i));
->>>>>>> registration
    
     blackMaskedImage = newImage;
     blackMaskedImage(~binaryImage) = 0;
     newCrop = imcrop(blackMaskedImage, [leftColumn, topLine, width, height]);
 
-<<<<<<< HEAD
-    cd ALGCropped    
-    imwrite(newCrop, sprintf('%04d.tif',x));    
-    x = x + 120; 
-=======
     cd(newLocation)    
     imwrite(newCrop, sprintf('%04d.tif',i));    
->>>>>>> registration
 end
+
