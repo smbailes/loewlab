9d6363e762737daec0838122521ab56cf03e34fe
Jill McGough <jmcgough@gwu.edu>
2018-06-08 14:22:13 -0400

    Updated demonsRegisteredEveryMinute to run on CPU

--- Symmetry Code (Final)/demonsRegisterEveryMinute.m
+++ Symmetry Code (Final)/demonsRegisterEveryMinute.m
@@ -6,7 +6,7 @@ mkdir(newLocation)
 
     %% Register Images
     fixed = imread([location '\0840.tif']); %reference image
-    fixedGPU = gpuArray(fixed); %Create gpuArray 
+%     fixedGPU = gpuArray(fixed); %Create gpuArray 
 %     fixed1 = getMatrixOutliers(fixed);
 %     fixed2 = fixed(find(fixed>0));
 %     high = max(fixed2);
@@ -18,19 +18,19 @@ mkdir(newLocation)
     for i = 120:120:1680
         imgpath = [location '\' sprintf('%04d.tif',i)];
         moving = imread(imgpath);
-        movingGPU = gpuArray(moving); %Create gpuArray
+%         movingGPU = gpuArray(moving); %Create gpuArray
 
 
         %Perform Registration
 %         [D, movingReg] = imregdemons(movingGPU, fixedGPU,200);
 %         [D, movingReg] = imregdemons(movingGPU, fixedGPU);
-        [~,movingReg] = imregdemons(movingGPU,fixedGPU,[500 400 200],'AccumulatedFieldSmoothing',3);
+        [~,movingReg] = imregdemons(moving,fixed,[500 400 200],'AccumulatedFieldSmoothing',3);
 %         [~,movingReg] = imregdemons(movingGPU,fixedGPU,[500 400 200],'PyramidLevels',3);
         
         %Bring registered image back to CPU
-        registeredImage = gather(movingReg);
+% %         registeredImage = gather(movingReg);
         
-        imwrite(registeredImage,sprintf('%04d.tif',i))
+        imwrite(movingReg,sprintf('%04d.tif',i))
         
         %Display registered images
 %         j = (i/120)+1;
