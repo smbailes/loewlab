561e6829503949a9768223770ab6544135418d6c
smbailes <39807025+smbailes@users.noreply.github.com>
2018-06-11 17:16:45 -0400

    Added file ROI_info
    Look at truth region for each patient

---
+++ Symmetry Code (Final)/ROI_info.m
@@ -0,0 +1,82 @@
+clear all;
+close all;
+clc;
+%% Patient Selection
+    [location, ptID] = pathfinder; 
+
+%% Image Input
+    
+    % Read 14 images to cell matrix I_mat
+    a=120;
+    for i=1:14          
+        I_mat{i} = imread([location sprintf('%04d.tif',a)]);    % Read each image into I_mat
+        a=a+120;            % Go to next image (for cropped)
+    end
+
+%% Import Truth Data from Excel file
+    [num,txt,raw] = xlsread('TruthData');   % Put TruthData into 3 cell arrays
+    index = find(ismember(txt,ptID));       % Select row where patient data is
+    hr = num(index-1,1);                    % Get clock hour from num file
+    dist = num(index-1,2);                  % Get distance from num file
+    xbox = num(index-1,3);                  % Get x-dim from num file
+    ybox = num(index-1,4);                  % Get y-dim from num file
+    sideString = txt(index,2);              % Get side from txt file
+    notes = txt(index,8);                   % Get any notes from txt file
+    celldisp(notes);   
+    
+%% ROI Identification
+% Convert Clock Hour to Angle (in rad) 
+    if hr <= 9 && hr > 3            
+        hr_ang = (abs((hr - 9)) * (pi/6)) + pi;
+    elseif hr <= 3 
+        hr_ang = (3 - hr) * (pi/6);
+    elseif hr > 9
+        hr_ang = pi - ((hr - 9) * (pi/6));
+    end
+    theta = hr_ang;     % angle in radians
+    
+    % Convert from Polar to Cartesian
+    scale = 15;             % Pixels / CM Scale (Can be changed to UI)
+    %Display pt ruler image and use ginput to get ruler size, change scale
+    %accordingly
+    rho = dist * scale;     % Distance from origin to ROI
+    [dx,dy] = pol2cart(theta, rho); % Convert tumor location as angle & dist to pixel location  
+    
+    
+    I1 = I_mat{1};              % Display first image
+    I = getMatrixOutliers(I1);  % Remove outliers
+    I_adj = I1(find(I1>0));    % Remove zero pixels
+    I_sort1 = sort(I_adj);
+    figure('Name','Nipple Identification')
+    imshow(I,[min(I_adj) max(I_adj)]);               % Display with contrast
+
+    if strcmp(sideString,'Left') == 1                   % Direct user to tumor side
+        xlabel('-->')
+    else
+        xlabel('<--')
+    end 
+    hold on
+    fprintf('Select Reference Nipple \n');  % User input of nipple region
+    [Xorg,Yorg] = ginput(1);
+    plot(Xorg,Yorg, '*');                   % Plot center of nipple
+    Xnew = Xorg + dx;                       % Coordinates of center of tumor
+    Ynew = Yorg - dy;    
+    xbox = xbox * scale;                    % Convert X and Y dimensions of tumor from cm to pixels
+    ybox = ybox * scale;                    % xbox and ybox are length of x and y sides in pixels
+    c1 = [round(Xnew - xbox/2), round(Ynew - ybox/2)];  % Top Left Corner
+    c2 = [round(Xnew - xbox/2), round(Ynew + ybox/2)];  % Bottom Left Corner
+    c3 = [round(Xnew + xbox/2), round(Ynew + ybox/2)];  % Bottum Right Corner
+    c4 = [round(Xnew + xbox/2), round(Ynew - ybox/2)];  % Top Right Corner      
+    hold off  
+    close    
+    
+%% Show Image and ROI
+for n = 1:14
+    imshow(I,[min(I_adj) max(I_adj)]);
+    hold on;
+    plot([c1(1) c2(1)],[c1(2) c2(2)],'r');                      % Create red box region on Image Display
+    plot([c2(1) c3(1)],[c2(2) c3(2)],'r');
+    plot([c3(1) c4(1)],[c3(2) c4(2)],'r');
+    plot([c4(1) c1(1)],[c4(2) c1(2)],'r');
+    hold off;
+end
