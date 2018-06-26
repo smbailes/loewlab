4cb1b7d203ef0cf79d3b96bc6a00c9d4c65f3454
samhitamurthy <31783706+samhitamurthy@users.noreply.github.com>
2018-06-26 16:41:26 -0400

    connect 2 biggest components

--- Segmentation MATLAB Code/zm_85_topline.m
+++ Segmentation MATLAB Code/zm_85_topline.m
@@ -16,8 +16,20 @@ biggest = bwareafilt(connected2,1,'largest');
 maxbig = max(bigarrayy); minbig = min(bigarrayy); 
 if abs(maxbig-minbig) < ((2/3)*co)
     biggest = bwareafilt(connected2,2,'largest');
-%     shapeInserter = vision.ShapeInserter('Shape', 'Lines', 'BorderColor', 'White','LineWidth',1);
-%     biggest = step(shapeInserter, biggest, uint16([yy1 xx1 yy2 xx2]));
+    shapeInserter = vision.ShapeInserter('Shape', 'Lines', 'BorderColor', 'White','LineWidth',1);
+    
+    xxnew = []; yynew = [];
+    [xxnew yynew] = find(biggest(:,1:(co/2)-1)==1); 
+    yy1 = max(yynew);
+    xx1 = xxnew(max(find(yynew == yy1))); 
+
+    xxnew = []; yynew = [];
+    [xxnew yynew] = find(biggest(:,(co/2):co)==1); 
+    yy2 = min(yynew);
+    xx2 = xxnew(max(find(yynew == yy2))); 
+    yy2 = min(yynew)+(co/2);
+
+    biggest = step(shapeInserter, biggest, uint16([yy1 xx1  yy2 xx2]));
 end
 % biggest(1:Yup, :) = 0;
 % biggest(Ylo:ro, :) = 0;
