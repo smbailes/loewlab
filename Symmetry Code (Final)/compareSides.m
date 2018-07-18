b1b449fa45f349685edc4e4311c89c54a3f2b414
smbailes <39807025+smbailes@users.noreply.github.com>
2018-07-18 11:50:11 -0400

    Updated compareSides

--- Symmetry Code (Final)/compareSides.m
+++ Symmetry Code (Final)/compareSides.m
@@ -44,40 +44,72 @@ defaultans = {'12','1','2'};          % default inputs
 options.Resize = 'on';                                                  % allows for resizing of box
 answer = inputdlg(prompt, dlg_title, [1 50], defaultans, options);      % creates box
 
-hr = str2double(answer{1});
-dist = str2double(answer{2});
-sqSize = str2double(answer{3});
+hrRight = str2num(answer{1});
+dist = str2num(answer{2});
+sqSize = str2num(answer{3});
 %% Create boxes
-if hr <= 9 && hr > 3            
-    hr_ang = (abs((hr - 9)) * (pi/6)) + pi;
-elseif hr <= 3 
-    hr_ang = (3 - hr) * (pi/6);
-elseif hr > 9
-    hr_ang = pi - ((hr - 9) * (pi/6));
+if hrRight <= 9 && hrRight > 3            
+    hr_ang = (abs((hrRight - 9)) * (pi/6)) + pi;
+elseif hrRight <= 3 
+    hr_ang = (3 - hrRight) * (pi/6);
+elseif hrRight > 9
+    hr_ang = pi - ((hrRight - 9) * (pi/6));
 end
-theta = hr_ang;     % angle in radians
+thetaRight = hr_ang;     % angle in radians
+
+if hrRight == 12
+    hrLeft = 12;
+else
+    hrLeft = 12-hrRight;
+end
+
+if hrLeft <= 9 && hrLeft > 3            
+    hr_ang1 = (abs((hrLeft - 9)) * (pi/6)) + pi;
+elseif hrLeft <= 3 
+    hr_ang1 = (3 - hrLeft) * (pi/6);
+elseif hrLeft > 9
+    hr_ang1 = pi - ((hrLeft - 9) * (pi/6));
+end
+thetaLeft = hr_ang1;     % angle in radians
 
 scale = 15;
 rho = dist * scale;     % Distance from origin to ROI
-[dx,dy] = pol2cart(theta, rho); % Convert tumor location as angle & dist to pixel location  
+[dxRight,dyRight] = pol2cart(thetaRight, rho); 
+[dxLeft,dyLeft] = pol2cart(thetaLeft, rho); % Convert tumor location as angle & dist to pixel location  
 sqSize = sqSize * scale;
 
+
 for j = 1:15
-    xRight = xR{j} + dx;                       % Coordinates of center of tumor
-    yRight = yR{j} - dy; 
-    xLeft = xL{j} + dx;
-    yLeft = yL{j} - dy;
+    xRight = xR{j} + dxRight;                       % Coordinates of center of tumor
+    yRight = yR{j} - dyRight; 
+    xLeft = xL{j} + dxLeft;
+    yLeft = yL{j} - dyLeft;
        
-    rightMinX = round(xRight - sqSize/2);
-    rightMinY = round(yRight - sqSize/2);
+    rightMinX{j} = round(xRight - sqSize/2);
+    rightMinY{j} = round(yRight - sqSize/2);
     
-    leftMinX = round(xLeft - sqSize/2);
-    leftMinY = round(yLeft - sqSize/2);
+    leftMinX{j} = round(xLeft - sqSize/2);
+    leftMinY{j} = round(yLeft - sqSize/2);
     
-    rightROI{j} = imcrop(I_mat{j}, [rightMinX rightMinY sqSize sqSize]);
-    leftROI{j} = imcrop(I_mat{j}, [leftMinX leftMinY sqSize sqSize]);
+    rightROI{j} = imcrop(I_mat{j}, [rightMinX{j} rightMinY{j} sqSize sqSize]);
+    leftROI{j} = imcrop(I_mat{j}, [leftMinX{j} leftMinY{j} sqSize sqSize]);
     
 end 
+
+%% 
+for m = 1:15
+    figure, 
+    I1 = I_mat{m}(find(I_mat{m}>0));
+    imshow(I_mat{m}, [min(I1) max(I1)])
+    hold on,
+    plot(rightMinX{m}, rightMinY{m}, '*');
+    hold on,
+    plot(xR{m}, yR{m}, '+');
+    hold on,
+    plot(leftMinX{m}, leftMinY{m}, '*');
+    hold on,
+    plot(xL{m}, yL{m}, '+');
+end
 
 %% Find data for ROI 
 
@@ -91,7 +123,7 @@ for l = 1:14
     stepChangeLeft(l) = aveLeft(l+1) - aveLeft(l);
 end
 
-fprintf('Clock Hour: %d \nDistance: %d \nSquare Size: %d \n',hr, dist, sqSize);
+fprintf('Clock Hour: %d \nDistance: %d \nSquare Size: %d \n',hrRight, dist, sqSize);
 
 totalChangeRight = aveRight(15) - aveRight(1)
 aveStepChangeRight = mean2(stepChangeRight)
