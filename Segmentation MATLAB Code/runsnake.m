ddc31fe29ea96c13c46f593a248e2e1eb4cd817e
elemieux19 <39834144+elemieux19@users.noreply.github.com>
2018-07-26 11:09:02 -0400

    getting better totals

--- Segmentation MATLAB Code/runsnake.m
+++ Segmentation MATLAB Code/runsnake.m
@@ -2,15 +2,5 @@
 close all;
 clc;
 
-%% Choose Image to input
-
-ptID = input('Enter image name you want to open: ','s'); %Request patient image name
-ptID = strcat(ptID,'.tif'); 
-%I = imread(['C:\Users\smbailes\Documents\GitHub\loewlab\Segmentation MATLAB Code\Images\' ptID]); %open the image, keeping it in 16-bits
-dir = uigetdir; 
-I = imread([dir '\' ptID]);
-
 %% Run Snakes
-
-snakimg = snake(I);
-imwrite(snakimg, 'SnakeImg_0000_P12.tif');
+snake(i);
