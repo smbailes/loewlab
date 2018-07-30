1ad0998919807275c1a1f3c50f2d2c86097611cd
Jill McGough <jmcgough@gwu.edu>
2018-07-30 00:08:11 -0400

    jaccard

--- Segmentation MATLAB Code/jaccard_practice.m
+++ Segmentation MATLAB Code/jaccard_practice.m
@@ -1,4 +1,4 @@
-% close all
+ close all
 % A = imread('0000 - P12.tif');
 % J=rgb2gray(A)
 % figure
@@ -16,18 +16,29 @@
 % figure
 % imshowpair(BW, BW_groundTruth)
 % title(['Jaccard Index = ' num2str(similarity)])
-J = imread('P4.tif');
+J = imread('Filled_0000_P12.tif');
+[x,y,z]=size(J)
+J(:,:,1)=[];
+J(:,:,2)=[];
+
+
+JJ=logical(J)
 figure
 imshow(J)
-title('connectPixels')
-mask = false(size(J));
-mask(25:end-25,25:end-25) = true;
-BW = activecontour(J, mask, 300);
-figure
-imshow(BW)
-title('Active Contour')
-M = imread('Cropped_0000_P4.tif');
+title('Snakes') %or connectPixels
+%mask = false(size(J));
+%mask(25:end-25,25:end-25) = true;
+%BW = activecontour(J, mask, 300);
+%figure
+%imshow(BW)
+%title('Active Contour')
+M = imread('Cropped_0000_P12a.tif');
 BW_groundTruth=logical(M)
-similarity = jaccard(J, BW_groundTruth);
 figure
-imshowpair(J, BW_groundTruth)
+imshow(BW_groundTruth);
+title('Truth')
+similarity = jaccard(JJ, BW_groundTruth);
+figure
+imshowpair(JJ, BW_groundTruth)
+j=similarity*100
+title('Jaccard Comparison for snakes and Truth.JIP=')
