1641cff6ad872b77eeae7c87e59a3a16abf1da9f
smbailes <39807025+smbailes@users.noreply.github.com>
2019-01-23 12:17:14 -0500

    Updated vesselness
    Go through different thicknesses and structure sensitivities

--- Volunteer and Patient Analysis/vesselness.m
+++ Volunteer and Patient Analysis/vesselness.m
@@ -23,16 +23,37 @@ for i = 1:20
     imshow(I_mat{n},[min(I) max(I)]);
     subplot(2,1,2);
     imshow(V)
+    xlabel(sprintf('Thickness = %f',i))
 end
 
-%% Show all fibermetrics on one subplot
 
-n = 8;
-image = I_mat{n};
-I = image(find(image>0));
-figure
-for i = 1:20
-    V = fibermetric(I_mat{n},i,'ObjectPolarity','bright','StructureSensitivity',12);
-    subplot(5,4,i);
+thickness = input('Best Thickness?');
+close all;
+
+for j = 1:30
+    V = fibermetric(I_mat{n},thickness,'ObjectPolarity','bright','StructureSensitivity',j);
+    figure, 
+    subplot(2,1,1);
+    imshow(I_mat{n},[min(I) max(I)]);
+    subplot(2,1,2);
     imshow(V)
+    xlabel(sprintf('Structure Sensitivity = %f',j))
 end
+% %% Show all fibermetrics on one subplot
+% 
+% n = 8;
+% image = I_mat{n};
+% I = image(find(image>0));
+% figure
+% for i = 1:20
+%     V = fibermetric(I_mat{n},i,'ObjectPolarity','bright','StructureSensitivity',12);
+%     subplot(5,4,i);
+%     imshow(V)
+% end
+
+%% Try CLAHE
+%  
+% J = adapthisteq(I_mat{1},'ClipLimit',0.02);
+% figure,
+% imshow(J,[min(min(J)) max(max(J))])
+% 
