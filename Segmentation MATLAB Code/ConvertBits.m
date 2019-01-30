08fed9b4d939c60a9ae44912a290f5369784b1b4
elemieux19 <39834144+elemieux19@users.noreply.github.com>
2019-01-30 14:12:08 -0500

    conversion updates

--- Segmentation MATLAB Code/ConvertBits.m
+++ Segmentation MATLAB Code/ConvertBits.m
@@ -1,3 +1,4 @@
+%% Conversion from 16 Bit to 8 Bit Images
 close all;
 
 ptID = input('Enter image name you want to open: ','s'); %Request patient image name
@@ -14,7 +15,36 @@ I(I == 0) = min(I(I > 0));
 J = (I - 8000);
 J = double(J/.2000);
 J = J*.255;
-
-figure;
-imshow(J, [])
 imwrite(J, sprintf('ConvertBit_0000_P1.tif'));
+%% Manual Crop of 8 Bit Images
+
+K = J(find(J > 0));
+figure, imshow(J, [])
+hold on
+hFH = imfreehand();
+binaryImage = hFH.createMask();
+xy = hFH.getPosition;
+
+% Get coordinates of the boundary of the freehand drawn region.
+structBoundaries = bwboundaries(binaryImage);
+xy=structBoundaries{1}; % Get n by 2 array of x,y coordinates.
+x = xy(:, 2);   % Columns.
+y = xy(:, 1);   % Rows.
+% Mask the image outside the mask, and display it.
+
+% Will keep only the part of the image that's inside the mask, zero outside mask.
+blackMaskedImage = I;
+blackMaskedImage(~binaryImage) = 0;
+
+% Now crop the image.
+leftColumn = min(x);
+rightColumn = max(x);
+topLine = min(y);
+bottomLine = max(y);
+width = rightColumn - leftColumn + 1;
+height = bottomLine - topLine + 1;    
+
+newCrop = imcrop(blackMaskedImage, [leftColumn, topLine, width, height]);
+close;
+
+imwrite(blackMaskedImage, sprintf('ManualCrop8Bit_0000_P1.tif')); 
