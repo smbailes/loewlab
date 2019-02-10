5f30ff4d323d82b9121d0268000efc25b6512a4a
Samhita <samhitamurthy@gwu.edu>
2019-02-10 13:34:40 -0500

    fixed cropManual

--- Segmentation MATLAB Code/cropManual.m
+++ Segmentation MATLAB Code/cropManual.m
@@ -4,7 +4,11 @@ I = imread(path1);
 I = imshow(I, []);
 hold on
 [J1, rect] = imcrop(I);
-imwrite(J1, sprintf('ex1.tif')); 
+[r c] = size(J1);
+numrows = 480 - r;
+J1 = padarray(J1, numrows, 0, 'pre');
+
+imwrite(J1, sprintf('0012_0000.tif')); 
 
 close all
 I = [];
@@ -14,9 +18,19 @@ I = [];
 path2 = uigetfile('.tif');
 I = imread(path2);
 J2 = imcrop(I, rect);
+J2 = padarray(J2, numrows, 0, 'pre');
 
-imwrite(J2, sprintf('ex2.tif')); 
+imwrite(J2, sprintf('0012_0900.tif')); 
 
+%% 
+
+
+path3 = uigetfile('.tif');
+I = imread(path3);
+J3 = imcrop(I, rect);
+J3 = padarray(J3, numrows, 0, 'pre');
+
+imwrite(J3, sprintf('0012_1799.tif')); 
 %% 
 
 figure(1)
