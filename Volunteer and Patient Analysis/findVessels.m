5733d8b37232d3dad56e756d26d655193f3d1b93
smbailes <39807025+smbailes@users.noreply.github.com>
2019-02-12 11:30:27 -0500

    Added some results from comparison

--- Volunteer and Patient Analysis/findVessels.m
+++ Volunteer and Patient Analysis/findVessels.m
@@ -1,6 +1,12 @@
+OTSU
+
 clear all, 
 close all
-
+%% Parameters:
+%Clip Limit
+%Theshold for vessels 
+CL = 0.025;
+thresh = 0.3; 
 %% Image Input
     
     % Read 14 images to cell matrix I_mat
@@ -13,12 +19,11 @@ close all
 image = I_mat{8};
 I = image(find(image>0));
 %% Try CLAHE
-CL = 0.025;
 J = adapthisteq(I_mat{8},'ClipLimit',CL,'NBins',double((max(max(image)))));
 
 %% Find vessels
 V1 = fibermetric(J,'ObjectPolarity','bright');
-V2 = V1>=0.2;
+V2 = V1>=thresh;
 
 %% Compare to 8-bit 
 I8 = imread('8bit.png');
@@ -27,29 +32,33 @@ img8 = I8(find(I8>0));
 J8 = adapthisteq(I8, 'ClipLimit', CL);
 
 V8 = fibermetric(J8, 'ObjectPolarity', 'bright');
-V8_1 = V8>=0.2;
+V8_1 = V8>=thresh;
 
-
-close all
-%% Show images
-subplot(3,1,1)
+%% Show images for comparison 
+figure,
+subplot(4,1,1)
 imshow(image,[min(I) max(I)])
 title('Original 16-bit image')
-subplot(3,1,2)
+subplot(4,1,2)
 imshow(J,[min(min(J)) max(max(J))])
 title('CLAHE Results for 16-bit image')
-subplot(3,1,3)
+subplot(4,1,3)
 imshow(V1)
 title('Vesselness for 16-bit CLAHE Result')
-
+subplot(4,1,4)
+imshow(V2)
+title('Thresholded Vessels')
 
 figure,
-subplot(3,1,1)
+subplot(4,1,1)
 imshow(I8,[min(img8) max(img8)])
 title('Original 8-bit image')
-subplot(3,1,2)
+subplot(4,1,2)
 imshow(J8,[min(min(J8)) max(max(J8))])
 title('CLAHE Results for 8-bit image')
-subplot(3,1,3)
+subplot(4,1,3)
 imshow(V8)
 title('Vesselness for 8-bit CLAHE Result')
+subplot(4,1,4)
+imshow(V8_1)
+title('Thresholded Vessels')
