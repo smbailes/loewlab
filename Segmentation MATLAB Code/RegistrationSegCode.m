93142e6b43f6b0e9f37b32885567fb5814d8b636
elemieux19 <39834144+elemieux19@users.noreply.github.com>
2019-03-08 14:38:54 -0500

    resizing

--- Segmentation MATLAB Code/RegistrationSegCode.m
+++ Segmentation MATLAB Code/RegistrationSegCode.m
@@ -7,6 +7,41 @@ I = imread([dir '\' ptID]);
 fixed = I_mat{8};
 cd(newLocation);
 
+fprintf('Pick right nipple. \n');
+figure, imshow(Ivis, []), title('Right Nipple Visible')
+[X1,Y1] = ginput(1);
+
+fprintf('Pick right nipple. \n');
+figure, imshow(Iref, []), title('Right Nipple Reference')
+[X2,Y2] = ginput(1);
+
+val = mean(mean(Ivis));
+
+% Cropping
+
+hshift = round(X2 - X1);
+if hshift>0
+    Ivis = padarray(Ivis, [0 abs(hshift)], val, 'pre');
+    Ivis = imcrop(Ivis, [0 0 640 512]);
+else
+    Ivis = padarray(Ivis, [0 abs(hshift)], val, 'post');
+    [r c] = size(Ivis);
+    Ivis = imcrop(Ivis, [(c-640) (512-r) (c) (r)]);
+end
+
+vshift = round(Y2 - Y1);
+if vshift>0
+    Ivis = padarray(Ivis, abs(vshift), val, 'pre');
+    Ivis = imcrop(Ivis, [0 0 640 512]);
+else
+    Ivis = padarray(Ivis, abs(vshift), val, 'post');
+    [r c] = size(Ivis);
+    Ivis = imcrop(Ivis, [(640-c) (r-511) c r]); 
+end
+
+figure(1), imshow(Ivis)
+figure(2), imshow(Iref, [])
+
 %Affines Registration
 JID = input('Enter image name for the moving image to open: ','s');
 JID = strcat(JID,'.tif');
