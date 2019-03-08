f331340eb4384bd9d6047639e660cf054a2f0ae3
elemieux19 <39834144+elemieux19@users.noreply.github.com>
2019-03-08 12:39:14 -0500

    fixing registration code

--- Segmentation MATLAB Code/RegistrationSegCode.m
+++ Segmentation MATLAB Code/RegistrationSegCode.m
@@ -1,51 +1,42 @@
+ptID = input('Enter image name you want to open: ','s'); %Request patient image name
+ptID = strcat(ptID,'.tif'); 
+%I = imread(['C:\Users\smbailes\Documents\GitHub\loewlab\Segmentation MATLAB Code\Images\' ptID]); %open the image, keeping it in 16-bits
+dir = uigetdir; 
+I = imread([dir '\' ptID]);
+
 fixed = I_mat{8};
 cd(newLocation);
 
-if strcmp(in, 'a')%Affine registration
-    for i = 0:120:1680
-    newFile = [location '/' sprintf('%04d.tif',i)];
-    newImage = imread(newFile);
+%Affines Registration
+JID = input('Enter image name for the moving image to open: ','s');
+JID = strcat(JID,'.tif');
+dir = uigetdir;
+newImage = imread([dir '\' JID]);
     
-    [optimizer, metric] = imregconfig('monomodal');
-    optimizer.MaximumStepLength = 0.04;
-    optimizer.MaximumIterations = 100; %SETTING FOR NEW MATLAB
+[optimizer, metric] = imregconfig('multimodal');
+optimizer.MaximumStepLength = 0.04;
+optimizer.MaximumIterations = 100; %SETTING FOR NEW MATLAB
 
-    registeredImage = imregister(newImage,fixed,'affine',optimizer,metric);
-    imwrite(registeredImage,sprintf('%04d.tif',i))
-    end
-    fprintf('Finished Affine Registration\n');
-elseif strcmp(in, 'd') %demons registration
-    
-    fixedGPU = gpuArray(fixed); %Create gpuArray
-    for i = 0:120:1680
-        imgpath = [location '\' sprintf('%04d.tif',i)];
-        moving = imread(imgpath);
+registeredImage = imregister(newImage,fixed,'affine',optimizer,metric);
+imwrite(registeredImage,sprintf('VisibleAff.tif',i))
+
+fprintf('Finished Affine Registration\n');
+
+%Demons Registration
+fixedGPU = gpuArray(fixed); %Create gpuArray
+movID = input('Enter image name for the moving image to open: ','s');
+movID = strcat(movID,'.tif');
+dir = uigetdir;
+moving = imread([dir '\' movID]);
 %         movingGPU = gpuArray(moving); %Create gpuArray
-        [~,movingReg] = imregdemons(moving,fixed,[500 400 200],'AccumulatedFieldSmoothing',3);
+[~,movingReg] = imregdemons(moving,fixed,[500 400 200],'AccumulatedFieldSmoothing',3);
 %         [~,movingReg] = imregdemons(movingGPU,fixedGPU,[500 400 200],'AccumulatedFieldSmoothing',3);
         
         %Bring registered image back to CPU
 %         registeredImage = gather(movingReg);
         
-        imwrite(movingReg,sprintf('%04d.tif',i))
-    end 
-    fprintf('Finished Demons Registration\n'); 
-end
+imwrite(movingReg,sprintf('VisibleDem.tif',i));
+fprintf('Finished Demons Registration\n');
 
-%% Show newly registered images
-n = 0;
-for j = 1:15
-    I_reg{j} = imread([newLocation sprintf('%04d.tif',n)]);
-    n = n+120;
-end
-
-for p = 1:15
-    figure
-    imshow(I_mat{p}, [l h]);
-end
 pause 
 close all;
-
-%% Crop Images
-RegisteredCropScript;
-pause, close all
