c1e019b0044466283bfb9a66bae1a698910738bd
Samhita <samhitamurthy@gwu.edu>
2019-04-03 18:15:06 -0400

    summary (required)

--- Segmentation MATLAB Code/cropIndividual.m
+++ Segmentation MATLAB Code/cropIndividual.m
@@ -1,24 +1,25 @@
-ptID = input('Enter image name you want to open: ','s'); %Request patient image name 
-    % ^ this will be 0000, 0120, 0240, 0360, 0480, 0600, 0720, 0840, 0960,
-    % 1080, 1200, 1320, 1440, 1560, 1680, 1800 or 1799
-ptID = strcat(ptID,'.tif'); %makes ptID the number you enter in "dot" tif (ex: 0000.tif)
-dir = uigetdir; %you will go into the folder with 8 bit images, then the patient number's folder and click open
-I = imread([dir '/' ptID]); %reads in the image from the folder you selected
+ptID = input('Enter image name you want to open: ','s'); % ENTER PATIENT IMAGE NUMBER
+%ex. 0000, 0120, 0240, 0360, 0480...1800 or 1799
+ptID = strcat(ptID,'.tif'); % ptID labelled as TIF file (ex. 0000.tif)
+dir = uigetdir; % select patient number's folder from folder with 8 bit images
+I = imread([dir '/' ptID]); % retrieves image from selected folder 
 I1 = I(find(I>0));
-imshow(I, []); %shows the image 
+
+imshow(I, []); % displays image 
 hold on
-hFH = imfreehand(); %this is when you draw the outline to crop
-%WHEN TRACING:
-    %1. start from the armpit on the left side of the screen (the patient's
-    %right) (hold down the mouse and drag)
-    %2. trace the outline of the underside of the breast 
-    %3. end at the armpit on the right side of the screen (the patient's
-    %left) (release the mouse it saves automatically
-binaryImage = hFH.createMask(); %creates a binary mask based on what you traced
-xy = hFH.getPosition; %gets the boundaries of what you traced
+hFH = imfreehand(); % DRAW OUTLINE TO MANUALLY SEGMENT
+% WHEN TRACING:
+    % 1. start from the armpit on the left side of the screen (the patient's
+    % right) (hold down the mouse and drag)
+    % 2. trace the outline of the underside of the breast 
+    % 3. end at the armpit on the right side of the screen (the patient's
+    % left) (release the mouse it saves automatically)
+    
+binaryImage = hFH.createMask(); % creates binary mask based on outline
+xy = hFH.getPosition; % gets boundaries of outline
 
 % Get coordinates of the boundary of the freehand drawn region.
-structBoundaries = bwboundaries(binaryImage); % Get coordinates of the boundary of the tracing
+structBoundaries = bwboundaries(binaryImage); % Get coordinates of the boundary of outline
 xy=structBoundaries{1}; % Get n by 2 array of x,y coordinates.
 x = xy(:, 2);   % Columns.
 y = xy(:, 1);   % Rows.
@@ -29,18 +30,16 @@ blackMaskedImage = I; %makes blackMaskedImage thats equal to the original image
 blackMaskedImage(~binaryImage) = 0; %this sets what was not in the traced region to 0 
 
 % Now crop the image.
-leftColumn = min(x); %this crops the image so that the left column in the minimum x value (for the traced region)
-rightColumn = max(x); %this crops the image so that the right column in the maximum x value (for the traced region)
-topLine = min(y); %this crops the image so that the top row is the minimum y value (for the traced region)
-bottomLine = max(y); %this crops the image so that the top row is the maximum y value (for the traced region)
-width = rightColumn - leftColumn + 1; %sets the width for the traced region 
-height = bottomLine - topLine + 1;  %sets the height for the traced region   
+leftColumn = min(x); % this crops the image so that the left column in the minimum x value (for the traced region)
+rightColumn = max(x); % this crops the image so that the right column in the maximum x value (for the traced region)
+topLine = min(y); % this crops the image so that the top row is the minimum y value (for the traced region)
+bottomLine = max(y); % this crops the image so that the top row is the maximum y value (for the traced region)
+width = rightColumn - leftColumn + 1; % sets the width for the traced region 
+height = bottomLine - topLine + 1;  % sets the height for the traced region   
 
 newCrop = imcrop(blackMaskedImage, [leftColumn, topLine, width, height]); %this crops teh blackmasked image to this size 
 close; 
 
-
-
-imwrite(blackMaskedImage, sprintf('hi_Manual3_P11.tif'));  %CHANGE THIS
-% ^ change to PatientNumber_ImageNumberM.tif (ex: for Patient 1 0000:
-% P1_0000M
+imwrite(blackMaskedImage, sprintf('PatientNumber_ImageNumberM.tif')); % CHANGE THIS BEFORE RUNNING CODE
+% format: 'PatientNumber_ImageNumberM.tif' 
+% ex. for Patient Number 1 and Image Number 0000: 'P1_0000M.tif'
